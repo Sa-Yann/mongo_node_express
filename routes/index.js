54167ae1414864cb580074cc6ca52e2452d2c797
Yann <knt4xyz@gmail.com>
2021-04-07 12:38:01 +0200

    📦 Creation app.js Avec Express add pug & sass

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,34 @@ var express = require('express');
 var router = express.Router();
 
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'MongoDB avec Express/node.js' });
+});
+
+// Code Step 1 : Confirmation de connection a la base de donnéee en  localhost:27017
+// import { MongoClient } from 'mongodb'
+let MongoClient = require('mongodb');
+
+async function connect () {
+  // Connection URL
+  const url = 'mongodb://localhost:27017/turlututu'
+
+  let db
+
+  try {
+    db = await MongoClient.connect(url)
+    console.log('Connected successfully!')
+  } catch (err) {
+    // on rajoute un console.log(err) pour afficher les erreurs si pas de connectoin
+    console.log(err);
+    // Handle error
+  }
+
+  return db
+}
+
+connect();
 
 // const MongoClient = require('mongodb').MongoClient;
 // const assert = require('assert');
@@ -22,9 +50,6 @@ var router = express.Router();
 //   client.close();
 // });
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MongoDB avec Express/node.js' });
-});
+
 
 module.exports = router;
