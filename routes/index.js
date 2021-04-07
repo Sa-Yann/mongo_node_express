6ee9bc2555db8b1386ae6ce526ae71c13252c59c
Yann <knt4xyz@gmail.com>
2021-04-07 14:06:56 +0200

    📝 Step 3 Insérer des données dans la bdd
    Step3 : fonction insertDocuments

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,90 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'MongoDB avec Express/node.js' });
 });
 
+
+
+// // Code Step 3 : Rajouts de la fonctio insertDocument pourrajouter des lements ala table `testcollection'
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+// const insertDocuments = require('../javascripts/insertDocuments.js')
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'admin';
+// Utilisation du constructeur MongoClient
+// Passer { useUnifiedTopology: true } en argument 
+// pour pouvoir échanger avec le serveur
+const client = new MongoClient(url, { useUnifiedTopology: true });
+// Use connect method to connect to the server
+client.connect(function(err) {
+  assert.strictEqual(null, err);
+  console.log('Connected successfully to server');
+
+  const db = client.db(dbName);
+  // on test insertDocuments en mettant bien le parametre db
+  insertDocuments (db)
+// CLient.colse() ferme la conncetionavant trop tot dc o comment ds un 1er temps
+  // client.close();
+});
+
+// fonction async insertDocument
+async function insertDocuments (db) {
+  // Get the documents collection
+  const collection = db.collection('testcollection')
+
+  // Insert some documents
+  const result = await collection.insertMany([
+    {
+      name: 'Sun Bakery Trattoria',
+      stars: 4,
+      categories: [
+        'Pizza', 'Pasta', 'Italian', 'Coffee', 'Sandwiches'
+      ]
+    }, {
+      name: 'Blue Bagels Grill',
+      stars: 3,
+      categories: [
+        'Bagels', 'Cookies', 'Sandwiches'
+      ]
+    },
+    {
+      id:220, 
+      first_name:"Saiyann",
+      last_name:"Grammer",
+      email:"saiyann0@columbia.edu",
+      gender:"Maler",
+      ip_address:"20.135.156.90",
+      Couleur_shirt:"Purple",
+    }
+  ])
+
+  return result
+}
+
+// // Code Step 2: Connect to MongoDB : https://www.npmjs.com/package/mongodb
+// const MongoClient = require('mongodb').MongoClient;
+// const assert = require('assert');
+
+// // Connection URL
+// const url = 'mongodb://localhost:27017';
+
+// // Database Name
+// const dbName = 'admin';
+// // Utilisation du constructeur MongoClient
+// // Passer { useUnifiedTopology: true } en argument 
+// // pour pouvoir échanger avec le serveur
+// const client = new MongoClient(url, { useUnifiedTopology: true });
+// // Use connect method to connect to the server
+// client.connect(function(err) {
+//   assert.strictEqual(null, err);
+//   console.log('Connected successfully to server');
+
+//   const db = client.db(dbName);
+
+//   client.close();
+// });
+
 // // Code Step 1 : Confirmation de connection a la base de donnéee en  localhost:27017
 // // link: https://www.mongodb.com/what-is-mongodb
 // // import { MongoClient } from 'mongodb'
@@ -34,28 +118,7 @@ router.get('/', function(req, res, next) {
 // connect();
 
 
-// Code Step 2: Connect to MongoDB : https://www.npmjs.com/package/mongodb
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'admin';
-// Utilisation du constructeur MongoClient
-// Passer { useUnifiedTopology: true } en argument 
-// pour pouvoir échanger avec le serveur
-const client = new MongoClient(url, { useUnifiedTopology: true });
-// Use connect method to connect to the server
-client.connect(function(err) {
-  assert.strictEqual(null, err);
-  console.log('Connected successfully to server');
-
-  const db = client.db(dbName);
-
-  client.close();
-});
 
 
 
